d966f3e1a92681f6917bf80314554f9b53d680ef
EdnaCamacho04 <edna_lizarraga11@hotmail.com>
2019-04-29 12:03:54 -0600

    Conexion a la base de datos

--- crud_node/src/rutas/index.js
+++ crud_node/src/rutas/index.js
@@ -3,6 +3,6 @@ const rutas = express.Router();
 
 //GET es lo que solicita el navegador del cliente
 rutas.get("/",(req,res) => {
-	res.send("Hola mundo Node");
+	res.send("Hola mundo Node"); 
 });
-module.exports = rutas;
+module.exports = rutas; 
