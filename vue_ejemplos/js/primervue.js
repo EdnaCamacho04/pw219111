274b8b1471d9b3edbc8fb388cc81e6e5b5b17350
EdnaCamacho04 <edna_lizarraga11@hotmail.com>
2019-05-07 12:07:05 -0600

    Primer vue

--- vue_ejemplos/js/primervue.js
+++ vue_ejemplos/js/primervue.js
@@ -1,6 +1,14 @@
 const app = new Vue({
 	el: "#app",
 	data: {
-		titulo: 'Hola mundo con VUE'
+		titulo: 'Hola mundo con VUE',
+		frutas: ['manzanas','peras','platano','mango'],
+		frutas2: [
+           {nombre:'manzana', cantidad: 10},
+           {nombre:'peras', cantidad: 0},
+           {nombre:'platano', cantidad: 20},
+           {nombre:'mango', cantidad: 80}
+
+		]
 	}
 });
